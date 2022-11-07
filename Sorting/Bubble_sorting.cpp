873345d5b3c9d2e04dc395e51172abbc439b4543
Keshav <keshavarshney18@gmail.com>
2022-11-07 14:33:14 +0530

    'bubble-sort'

--- Sorting/Bubble_sorting.cpp
+++ Sorting/Bubble_sorting.cpp
@@ -1,1 +1,25 @@
-#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+//bubble sort
+//Time complexity --> O(N2)
+void bubbleSort(int arr[], int n)
+{
+	int i, j;
+	for (i = 0; i < n - 1; i++){
+		for (j = 0; j < n - i - 1; j++){
+			if (arr[j] > arr[j + 1])
+				swap(arr[j], arr[j + 1]);
+        }
+    }
+}
+int main()
+{
+	int arr[] = {1,32,10,8,23,34,15};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	bubbleSort(arr, n);
+	cout << "Sorted array: \n";
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+	return 0;
+}
